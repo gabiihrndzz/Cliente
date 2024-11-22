b4c0085440c4d8e653175df3dc69002bac65bfc4
Gabriel <gabriel.hdezcorrea@gmail.com>
2024-11-22 12:23:05 +0100

    actualizacion

---
+++ 1Evaluacion/Tema4/Prueba/tema4.js
@@ -0,0 +1,6 @@
+function saludarUnaVez(){
+    alert('Hola,Mnudo');
+}
+const colorear = () => document.getElementById('w3c').style.color='red';
+
+const fondo = () => document.getElementById('w3c').style.background= 'blue';
